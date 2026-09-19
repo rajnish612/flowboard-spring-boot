aba542d4e9521453cebc554305ff1d7548266fd5
Rajnish Nath <rnish612@gmail.com>
2026-09-19 15:32:52 +0530

    feat: remove card-related fields from NotificationEvent class

--- taskservice/src/main/java/com/server/taskservice/dto/NotificationEvent.java
+++ taskservice/src/main/java/com/server/taskservice/dto/NotificationEvent.java
@@ -25,9 +25,7 @@ public class NotificationEvent {
     private Long boardId;
     private String boardName;
 
-    // Card related to the action, if any.
-    private Long cardId;
-    private String cardTitle;
+
 
     // Type of notification.
     private NotificationType type;
